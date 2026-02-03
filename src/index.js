ce4cd3b982e3157601446fa89394a7df88996ee9
gaurav-soni34 <gauravsoni1967@gmail.com>
2026-02-03 19:54:11 +0530

    connect to monho atlas

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// import mongoose from "mongoose"
+// import {DB_NAME} from "./constant"
+
+
+
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+    dotenv.config({
+        path:'./env'
+    })
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app=express()
+
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error",(error)=>{
+            console.log("ERROR :",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on post ${process.env.PORT}`);     
+        })
+    }
+    catch(error){
+        console.log("ERROR",error)
+        throw error
+    }
+})()*/
